0890321bbfa8e0e9b5db7a5363281b700309775f
DESKTOP-8106DVP\User <anderson.case13@gmail.com>
2025-12-27 19:40:46 -0300

    Criando Dtos de Organizacao e Usuario

---
+++ Shared/InfraDocs.Shared/Dtos/Usuario/CreateUsuarioDto.cs
@@ -0,0 +1,30 @@
+﻿using InfraDocs.Shared.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace InfraDocs.Shared.Dtos.Usuario
+{
+    public class CreateUsuarioDto
+    {
+        [Required(ErrorMessage = "A organização do usuário é obrigatória.")]
+        public int OrganizacaoId { get; set; }
+
+        [Required(ErrorMessage = "O nome do usuário é obrigatório.")]
+        public string Nome { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "O e-mail do usuário é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O e-mail informado não é válido.")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "O documento do usuário é obrigatório.")]
+        public string Documento { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "O tipo de documento é obrigatório.")]
+        public TipoDocumentoEnum TipoDocumento { get; set; }
+
+        [Required(ErrorMessage = "O tipo de usuário é obrigatório.")]
+        public TipoUsuarioEnum TipoUsuario { get; set; }
+
+        [Required(ErrorMessage = "O status do usuário é obrigatório.")]
+        public StatusBasicoEnum StatusUsuario { get; set; }
+    }
+}
---
+++ Shared/InfraDocs.Shared/Dtos/Usuario/ReadUsuarioDto.cs
@@ -0,0 +1,16 @@
+﻿using InfraDocs.Shared.Enums;
+
+namespace InfraDocs.Shared.Dtos.Usuario
+{
+    public class ReadUsuarioDto
+    {
+        public int Id { get; set; }
+        public int OrganizacaoId { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Documento { get; set; } = string.Empty;
+        public TipoDocumentoEnum TipoDocumento { get; set; }
+        public TipoUsuarioEnum TipoUsuario { get; set; }
+        public StatusBasicoEnum StatusUsuario { get; set; }
+    }
+}
